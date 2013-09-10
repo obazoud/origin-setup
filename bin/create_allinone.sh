62829bda1c9e2c7dce81fd7fa0c19bdb6734a16c
Mark Lamourine <markllama@gmail.com>
2013-09-10 10:06:12 -0400

    have cases for setting up fedora 19, rhel6.4 and centos6.4. need to generalize

--- bin/create_allinone.sh
+++ bin/create_allinone.sh
@@ -16,22 +16,43 @@ BASEOS=$2
 BASEOS=${BASEOS:="rhel6.4"} # | fedora19 | centos6.4
 
 AWSTYPE=$3
-AWSTYPE=${AWSTYPE="t1.micro"} # | m1.small|m1.medium|m1.large
+AWSTYPE=${AWSTYPE:="t1.micro"} # | m1.small|m1.medium|m1.large
+
+AWS_CONFIG_FILE=${AWS_CONFIG_FILE:="./.awscred"}
+REMOTE_USER=$(ruby -e "require 'parseconfig' ; puts ParseConfig.new('$AWS_CONFIG_FILE')['$BASEOS']['RemoteUser']")
+REMOTE_USER=${REMOTE_USER:=fedora}
+
+# make it right regardless
+if [ -n "$VERBOSE" ] ; then
+    VERBOSE="--verbose"
+fi
 
 # origin:baseinstance
 thor origin:baseinstance $NAME --baseos ${BASEOS} --type ${AWSTYPE} ${VERBOSE}
+
 HOSTNAME=$(thor ec2:instance hostname --name $NAME)
 # origin:prepare
-#thor origin:prepare $HOSTNAME 
+
+PUPPET_RPM=puppet
+
+case $BASEOS in
+    "rhel6.4")
+        # "manually" add deps repo to provide puppet2
+        thor origin:depsrepo $HOSTNAME --username ${REMOTE_USER} ${VERBOSE}
+        PUPPET_RPM="puppet2"
+        ;;
+    
+    "centos6.4")
+        # enable epel for puppet
+        thor remote:yum:install $HOSTNAME http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm --username ${REMOTE_USER} ${VERBOSE}
+        ;;
+
+    *)
+        
+esac
+
+thor origin:prepare $HOSTNAME --packages git ${PUPPET_RPM} --username ${REMOTE_USER} ${VERBOSE}
 
 
 
-#hostname=$(thor ec2:instance hostname --id $instanceid)
 
-# "manually" add deps repo to provide puppet2
-#thor origin:depsrepo $hostname
-
-# install puppet2 and git
-#thor remote:yum install $hostname puppet2 git
-
-# pull down git repo for puppet modules
